54135c38fe1e8d3cc52f46821dae82dd57e550fd
bbarkse93 <bbarkse93@gmail.com>
2023-11-10 14:43:58 +0900

    세팅 완료

--- src/test/java/com/example/restdocapp/user/UserControllerTest.java
+++ src/test/java/com/example/restdocapp/user/UserControllerTest.java
@@ -1,22 +1,19 @@
 package com.example.restdocapp.user;
 
+import com.example.restdocapp.MyWithRestDoc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @SpringBootTest // 통합테스트
-public class UserControllerTest {
-
-    @Autowired
-    private MockMvc mvc;
+public class UserControllerTest extends MyWithRestDoc {
 
     @Test
     public void join_test() throws Exception {
@@ -27,9 +24,12 @@ public class UserControllerTest {
         requestDTO.setEmail("cos@nate.com");
         ObjectMapper om = new ObjectMapper();
         String requestBody = om.writeValueAsString(requestDTO);
+        System.out.println("===========================================================================");
+        System.out.println("||  🐛  "+requestBody+"    ||");
+        System.out.println("===========================================================================");
 
         // when
-       ResultActions resultActions = mvc.perform(
+       ResultActions resultActions = mockMvc.perform(
                 MockMvcRequestBuilders
                         .post("/join")
                         .content(requestBody)
@@ -40,10 +40,42 @@ public class UserControllerTest {
 
 
         // then
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.username").value("cos"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.password").value("1234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.email").value("cos@nate.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error").isEmpty())
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(document);
+
     }
 
     @Test
-    public void userInfo_test(){
+    public void userInfo_test() throws Exception {
+        // given
+        int id = 1;
 
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/users/"+id)
+        );
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.username").value("ssar"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.password").value("1234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.email").value("ssar@nate.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error").isEmpty())
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(document);
     }
 }
