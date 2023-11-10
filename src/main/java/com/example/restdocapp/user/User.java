f09d8a1c806178b4fb2ba8512ce4ee0b7de0fec2
bbarkse93 <bbarkse93@gmail.com>
2023-11-10 11:12:53 +0900

    first commit

---
+++ src/main/java/com/example/restdocapp/user/User.java
@@ -0,0 +1,37 @@
+package com.example.restdocapp.user;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 빈 생성자를 호출할 수 없게 해줌
+@Table(name = "user_tb")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, length = 20)
+    private String username;
+    @Column(length = 100)
+    private String password;
+    @Column(length = 100)
+    private String email;
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+    @Builder
+    public User(Integer id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+}
